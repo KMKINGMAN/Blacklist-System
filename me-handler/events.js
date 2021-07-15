9fcac627b7838e22b840316a3558eddfcfb14349
MeKINGMAN <84540534+MeKINGMAN@users.noreply.github.com>
2021-07-15 23:51:20 +0300

    Fixed

--- me-handler/events.js
+++ me-handler/events.js
@@ -45,7 +45,6 @@ module.exports = (client) => {
         table.addRow(file, '✅');
         client.on(event.name, (...args) => event.execute(...args, client));
         }else{
-          client.on(event.name, (...args) => event.execute(...args, client));
           table.addRow(file, `❌`);
           continue;
         }
