5b8b905d84202d1607cc51a87cbc2c54d18bf5cf
KMKINGMAN <84540534+KMKINGMAN@users.noreply.github.com>
2021-08-31 11:00:43 -0400

    Update help.js

--- command/devloper/help.js
+++ command/devloper/help.js
@@ -9,7 +9,7 @@ const { MessageEmbed } = require("discord.js");
    run: async (client, kmsg, args, PREFIX) => {
     let commands = kmsg.client.commands.array();
     let EMBED = new MessageEmbed()
-    .setTitle(`MeCodes BlackList System`)
+    .setTitle(`KINGMANDEV BlackList System`)
     .setDescription(`**[HelpMenu](https://github.com/KMKINGMAN)**`)
     .setColor("GOLD");
     commands.forEach((cmd) => {
